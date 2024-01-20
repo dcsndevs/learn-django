26d62aa6a8942bed83312ec3b6e2ce7bac3ee4f2
Dickson Olejeme <olejemedickson@gmail.com>
2024-01-20 03:55:36 +0000

    Testing Debug

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ieq))80ax9=0w6hk@8@g(*_qbsj=#b-q@^+-^7^kry$+9ieifk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-dcsndevs-learndjango-6jfhqknzecv.ws-eu107.gitpod.io','127.0.0.1', 'localhost', '.herokuapp.com']
 
