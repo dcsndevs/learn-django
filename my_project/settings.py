acef78837b3f5f47321b82ba178fd23ff83c3b5c
Dickson Olejeme <olejemedickson@gmail.com>
2024-01-20 02:46:59 +0000

    Add about_me URL

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ieq))80ax9=0w6hk@8@g(*_qbsj=#b-q@^+-^7^kry$+9ieifk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-dcsndevs-learndjango-6jfhqknzecv.ws-eu107.gitpod.io','127.0.0.1']
+ALLOWED_HOSTS = ['8000-dcsndevs-learndjango-6jfhqknzecv.ws-eu107.gitpod.io','127.0.0.1', 'localhost']
 
 
 # Application definition
